6e0e584ecd52d3e2f41f3c2009006aac2c8a03d1
Marcelo Belli <marcelo@belli.me>
2017-10-17 21:46:46 -0200

    Created BaseModel

--- work-at-olist/core/models.py
+++ work-at-olist/core/models.py
@@ -1,3 +1,19 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+
+class BaseModel(models.Model):
+    """
+    Base model for Channel and Category
+    """
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(_('Name'), max_length=50)
+    slug = models.SlugField(_('Slug'))
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        abstract = True
