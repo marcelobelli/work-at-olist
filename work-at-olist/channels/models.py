6cc95e44af47db7beb87b92c904ba7fb43831c74
Marcelo Belli <marcelo@belli.me>
2017-10-17 21:55:39 -0200

    Created channel model

--- work-at-olist/channels/models.py
+++ work-at-olist/channels/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils.text import slugify
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+from core.models import BaseModel
+
+
+class Channel(BaseModel):
+    """
+    Channel Model
+    """
+    name = models.CharField(_('Name'), max_length=50, unique=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+
+        super(Channel, self).save()
