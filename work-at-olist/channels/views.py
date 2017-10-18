372e6373ab63e4b04b9b93c3ff08fa791eb3905e
Marcelo Belli <marcelo@belli.me>
2017-10-17 22:29:21 -0200

    Created channels views

--- work-at-olist/channels/views.py
+++ work-at-olist/channels/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ReadOnlyModelViewSet
 
-# Create your views here.
+from .models import Channel
+from .serializers import ChannelDetailSerializer, ChannelListSerializer
+
+
+class ChannelViewSet(ReadOnlyModelViewSet):
+    """
+    Viewset used to show channels list and detail
+    """
+    lookup_field = 'slug'
+    queryset = Channel.objects.all()
+
+    def get_serializer_class(self):
+        serializers = {
+            'list': ChannelListSerializer,
+            'retrieve': ChannelDetailSerializer,
+        }
+        return serializers[self.action]
