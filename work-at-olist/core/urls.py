beb06df3718b3a176222af6d65abecf78760214f
Marcelo Belli <marcelo@belli.me>
2017-10-18 12:50:45 -0200

    Add categories router

--- work-at-olist/core/urls.py
+++ work-at-olist/core/urls.py
@@ -1,8 +1,10 @@
 from rest_framework import routers
 
+from categories.views import CategoryViewSet
 from channels.views import ChannelViewSet
 
 router = routers.SimpleRouter()
 router.register(r'channels', ChannelViewSet)
+router.register(r'categories', CategoryViewSet)
 
 urlpatterns = router.urls
