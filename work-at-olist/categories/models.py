281e343e7a6eb6e8135eeaadd05d7b5a485d9068
Marcelo Belli <marcelo@belli.me>
2017-10-17 23:24:34 -0200

    Created category model

--- work-at-olist/categories/models.py
+++ work-at-olist/categories/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.utils.text import slugify
+from django.utils.translation import ugettext_lazy as _
+from mptt.models import MPTTModel, TreeForeignKey
 
-# Create your models here.
+from core.models import BaseModel
+
+
+class Category(BaseModel, MPTTModel):
+    channel = models.ForeignKey(
+        'channels.Channel',
+        verbose_name=_('Channel'),
+        related_name='categories',
+        on_delete=models.CASCADE
+    )
+
+    parent = TreeForeignKey(
+        'self',
+        verbose_name=_('Parent'),
+        related_name='children',
+        on_delete=models.CASCADE,
+        db_index=True,
+        null=True,
+        blank=True
+    )
+
+    def save(self, *args, **kwargs):
+
+        if self.parent:
+            slug = slugify(f'{self.channel.slug}-{self.parent.name}-{self.name}')
+        else:
+            slug = slugify(f'{self.channel.slug}-{self.name}')
+        self.slug = slug
+
+        super(Category, self).save()
