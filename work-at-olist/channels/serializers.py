bce477cb15ee791ebf7bdf179138df45aaeaf6e3
Marcelo Belli <marcelo@belli.me>
2017-10-17 22:39:48 -0200

    Add url field and refactored channels serializers

--- work-at-olist/channels/serializers.py
+++ work-at-olist/channels/serializers.py
@@ -7,15 +7,23 @@ class ChannelListSerializer(serializers.ModelSerializer):
     """
     Serializer for channel data
     """
+    url = serializers.HyperlinkedIdentityField(
+        view_name='api:channel-detail',
+        lookup_field='slug'
+    )
+    channel = serializers.StringRelatedField(source='name')
+
     class Meta:
         model = Channel
-        fields = ('name', )
+        fields = ('channel', 'url')
 
 
 class ChannelDetailSerializer(serializers.ModelSerializer):
     """
     Serializer used to show channels detail
     """
+    channel = serializers.StringRelatedField(source='name')
+
     class Meta:
         model = Channel
-        fields = ('name', )
+        fields = ('channel', )
