0abe42275aecb1d3877b3d36c9487218d4e9e90f
Marcelo Belli <marcelo@belli.me>
2017-10-18 23:37:42 -0200

    Add tests for import_categories method

--- work-at-olist/categories/tests/test_categories_models.py
+++ work-at-olist/categories/tests/test_categories_models.py
@@ -5,10 +5,30 @@ from ..models import Category
 
 
 class ChannelModelTest(TestCase):
+    def setUp(self):
+        self.channel = Channel.objects.create(**{'name': 'Big Market'})
+        self.categories = ['Video Game', 'Playstation 4']
 
     def test_slug_creation(self):
         """slug must be created automatically after category creation"""
-        channel = Channel.objects.create(**{'name': 'Big Market'})
-        category = Category.objects.create(**{'name': 'Video game', 'channel': channel})
+
+        category = Category.objects.create(**{'name': 'Video game', 'channel': self.channel})
 
         self.assertTrue(category.slug)
+
+    def test_import_categories_method(self):
+        """Method must imports categories from a list to given channel"""
+        Category.import_categories(self.channel, self.categories)
+
+        result = Category.objects.count()
+
+        self.assertEqual(result, len(self.categories))
+
+    def test_import_categories_parent_children_relationship(self):
+        """In a list, the first category must be the parent of the second"""
+        Category.import_categories(self.channel, self.categories)
+
+        parent = Category.objects.get(name=self.categories[0])
+        children = Category.objects.get(name=self.categories[1])
+
+        self.assertEqual(parent, children.parent)
