e465a1f27c685e35d1fb2eee6a8a016775a9dee5
Marcelo Belli <marcelo@belli.me>
2017-10-20 15:49:32 -0200

    Add retrieve method to make the new slug work

--- work-at-olist/categories/views.py
+++ work-at-olist/categories/views.py
@@ -1,3 +1,5 @@
+from rest_framework.generics import get_object_or_404
+from rest_framework.response import Response
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
 from .models import Category
@@ -11,3 +13,15 @@ class CategoryViewSet(ReadOnlyModelViewSet):
     lookup_field = 'slug'
     serializer_class = CategorySerializer
     queryset = Category.objects.all()
+
+    def retrieve(self, request, *args, **kwargs):
+        channel_slug = kwargs['channel_slug']
+        slug = kwargs['slug']
+        queryset = Category.objects.filter(
+            channel__slug=channel_slug,
+            slug=slug
+        )
+        category = get_object_or_404(queryset)
+        serializer = CategorySerializer(category, context={'request': request})
+
+        return Response(serializer.data)
